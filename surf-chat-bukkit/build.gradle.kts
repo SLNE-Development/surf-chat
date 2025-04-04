51b2827be5abc7a83a5766f46340e568163007eb
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-04-04 13:08:14 +0200

    feat: configure SurfPaperPlugin with main class and authors

--- surf-chat-bukkit/build.gradle.kts
+++ surf-chat-bukkit/build.gradle.kts
@@ -1,3 +1,8 @@
 plugins {
     id("dev.slne.surf.surfapi.gradle.paper-plugin")
 }
+
+surfPaperPluginApi {
+    mainClass("dev.slne.surf.chat.bukkit.SurfChatBukkit")
+    authors.add("SLNE Development")
+}

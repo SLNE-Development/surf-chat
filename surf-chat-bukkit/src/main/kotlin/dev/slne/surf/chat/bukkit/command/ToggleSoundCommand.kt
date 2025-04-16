4a24c73caf1df2db5ca2b9637603bc45a8286f71
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-04-16 22:08:39 +0200

    feat: add ToggleSoundCommand to manage sound notification preferences

---
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/ToggleSoundCommand.kt
@@ -0,0 +1,34 @@
+package dev.slne.surf.chat.bukkit.command
+
+import com.github.shynixn.mccoroutine.folia.launch
+import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.kotlindsl.playerExecutor
+import dev.slne.surf.chat.bukkit.plugin
+import dev.slne.surf.chat.bukkit.util.sendText
+import dev.slne.surf.chat.core.service.databaseService
+import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
+import net.kyori.adventure.text.format.TextDecoration
+
+class ToggleSoundCommand(commandName: String) : CommandAPICommand(commandName) {
+    init {
+        withPermission("surf.chat.command.toggle.notify")
+        playerExecutor { player, _ ->
+            plugin.launch {
+                val user = databaseService.getUser(player.uniqueId)
+                val likesSound = user.toggleSound()
+
+                if(likesSound) {
+                    user.sendText(buildText {
+                        primary("Du hörst jetzt ")
+                        success("Notify-Sounds. ")
+                    })
+                } else {
+                    user.sendText(buildText {
+                        primary("Du hörst jetzt ")
+                        success("keine Notify-Sounds mehr.")
+                    })
+                }
+            }
+        }
+    }
+}

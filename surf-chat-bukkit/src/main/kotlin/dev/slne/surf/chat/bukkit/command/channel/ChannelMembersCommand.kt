03b3619846bf3c4f23c8a8bdf3b62b2657ae30b1
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-05-12 15:05:40 +0200

    feat: improve channel member command with optional channel argument and enhanced error messaging

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelMembersCommand.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelMembersCommand.kt
@@ -1,13 +1,12 @@
 package dev.slne.surf.chat.bukkit.command.channel
 
-import com.github.shynixn.mccoroutine.folia.launch
 import dev.jorel.commandapi.CommandAPICommand
 import dev.jorel.commandapi.kotlindsl.integerArgument
 import dev.jorel.commandapi.kotlindsl.playerExecutor
 
 import dev.slne.surf.chat.api.model.ChannelModel
 import dev.slne.surf.chat.api.surfChatApi
-import dev.slne.surf.chat.bukkit.plugin
+import dev.slne.surf.chat.bukkit.command.argument.ChannelArgument
 import dev.slne.surf.chat.bukkit.util.PageableMessageBuilder
 import dev.slne.surf.chat.core.service.channelService
 import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
@@ -15,20 +14,21 @@ import net.kyori.adventure.text.format.TextDecoration
 
 class ChannelMembersCommand(commandName: String) : CommandAPICommand(commandName) {
     init {
+        withArguments(ChannelArgument("channel").setOptional(true))
         integerArgument("page", 1, Int.MAX_VALUE, true)
         playerExecutor { player, args ->
             val page = args.getOrDefaultUnchecked("page", 1)
-            val channel: ChannelModel? = channelService.getChannel(player)
+            val channel: ChannelModel? = args.getOrDefaultUnchecked("channel", channelService.getChannel(player))
 
             if (channel == null) {
                 surfChatApi.sendText(player, buildText {
-                    error("Du bist in keinem Nachrichtenkanal.")
+                    error("Du bist in keinem Nachrichtenkanal oder der Kanal existiert nicht.")
                 })
                 return@playerExecutor
             }
 
             PageableMessageBuilder {
-                pageCommand = "/channel members %page%"
+                pageCommand = "/channel members ${channel.name} %page%"
 
                 title {
                     info("ᴍɪᴛɢʟɪᴇᴅᴇʀ ᴠᴏɴ ")

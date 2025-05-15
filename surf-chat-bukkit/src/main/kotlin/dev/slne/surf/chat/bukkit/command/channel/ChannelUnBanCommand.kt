3eae860c6baa0834c2356472dc1f1257f0187d56
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-05-15 14:06:10 +0200

    feat: add error handling for channel absence in ChannelUnBanCommand

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelUnBanCommand.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelUnBanCommand.kt
@@ -5,6 +5,7 @@ import dev.jorel.commandapi.CommandAPICommand
 import dev.jorel.commandapi.arguments.EntitySelectorArgument
 import dev.jorel.commandapi.kotlindsl.playerExecutor
 import dev.slne.surf.chat.api.model.ChannelModel
+import dev.slne.surf.chat.api.surfChatApi
 import dev.slne.surf.chat.bukkit.plugin
 import dev.slne.surf.chat.bukkit.util.ChatPermissionRegistry
 import dev.slne.surf.chat.bukkit.util.utils.sendText
@@ -21,16 +22,16 @@ class ChannelUnBanCommand(commandName: String) : CommandAPICommand(commandName) 
             val target = args.getUnchecked<Player>("player") ?: return@playerExecutor
             val channel: ChannelModel? = channelService.getChannel(player)
 
+            if(channel == null) {
+                surfChatApi.sendText(player, buildText {
+                    error("Du bist in keinem Nachrichtenkanal.")
+                })
+                return@playerExecutor
+            }
+
             plugin.launch {
                 val user = databaseService.getUser(player.uniqueId)
                 val targetUser = databaseService.getUser(target.uniqueId)
-
-                if (channel == null) {
-                    user.sendText(buildText {
-                        error("Du bist in keinem Nachrichtenkanal.")
-                    })
-                    return@launch
-                }
 
                 if (!channel.isModerator(user)) {
                     user.sendText(buildText {

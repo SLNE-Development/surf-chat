5a7ab7223340b7a41bff22e7d78c4e85ac9f767a
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-20 19:47:20 +0200

    feat: implement getChannel method in FallbackChannelService with a TODO for future implementation

--- surf-chat-fallback/src/main/kotlin/dev/slne/surf/chat/fallback/service/FallbackChannelService.kt
+++ surf-chat-fallback/src/main/kotlin/dev/slne/surf/chat/fallback/service/FallbackChannelService.kt
@@ -1,6 +1,10 @@
 package dev.slne.surf.chat.fallback.service
 
 import dev.slne.surf.chat.core.service.ChannelService
+import java.util.*
 
 class FallbackChannelService : ChannelService {
+    override fun getChannel(channelUuid: UUID) {
+        TODO("Not yet implemented")
+    }
 }

d16b234b48e61f7767b80e791ddaec62c49a30ff
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-20 19:43:26 +0200

    feat: enhance AsyncChatListener with message formatting and validation

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/listener/AsyncChatListener.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/listener/AsyncChatListener.kt
@@ -1,28 +1,35 @@
 package dev.slne.surf.chat.bukkit.listener
 
-import com.github.benmanes.caffeine.cache.Caffeine
-import com.sksamuel.aedile.core.expireAfterWrite
+import dev.slne.surf.chat.bukkit.message.MessageFormatterImpl
+import dev.slne.surf.chat.bukkit.message.MessageValidatorImpl
+import dev.slne.surf.chat.bukkit.util.cancel
+import dev.slne.surf.chat.bukkit.util.user
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
 import io.papermc.paper.event.player.AsyncChatEvent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import kotlin.time.Duration.Companion.minutes
 
 class AsyncChatListener : Listener {
-    private val pingPattern = Regex("^@(all|a|here|everyone)\\b\\s*", RegexOption.IGNORE_CASE)
-    private val hexRegex = Regex("&#[A-Fa-f0-9]{6}")
-    private val linkRegex = Regex("(?i)\\b((https?://)?[\\w-]+(\\.[\\w-]+)+(/\\S*)?)\\b")
-    private val itemRegex = Regex("\\[(?i)item]")
-    private val nameRegexCache = Caffeine.newBuilder()
-        .expireAfterWrite(15.minutes)
-        .build<String, Regex> {
-            Regex("(?<!\\w)@?$it(?!\\w)")
-        }
-
     @EventHandler
     fun onAsyncChat(event: AsyncChatEvent) {
         val player = event.player
-        val message = event.message()
+        val user = player.user() ?: return
 
+        val message = event.message()
+        val messageFormatter = MessageFormatterImpl(message)
+        val messageValidator = MessageValidatorImpl.componentValidator(message)
+
+
+        if (!messageValidator.validate(user)) {
+            event.cancel()
+
+            player.sendText {
+                messageValidator.failureMessage
+            }
+            return
+        }
+
+        val formattedMessage = messageFormatter.format()
 
         event.renderer { _, displayName, _, viewerAudience ->
             message

59e51abc0572977b817a83a8ee12cf3d303290cc
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-20 19:43:34 +0200

    feat: update build configuration and enhance UserService with singleton instance

--- surf-chat-bukkit/build.gradle.kts
+++ surf-chat-bukkit/build.gradle.kts
@@ -2,8 +2,13 @@ plugins {
     id("dev.slne.surf.surfapi.gradle.paper-plugin")
 }
 
+repositories {
+    maven("https://repo.extendedclip.com/releases/")
+}
+
 dependencies {
     api(project(":surf-chat-core"))
+    compileOnly(libs.playerholder.api)
 }
 
 surfPaperPluginApi {
--- surf-chat-core/src/main/kotlin/dev/slne/surf/chat/core/service/UserService.kt
+++ surf-chat-core/src/main/kotlin/dev/slne/surf/chat/core/service/UserService.kt
@@ -1,9 +1,16 @@
 package dev.slne.surf.chat.core.service
 
 import dev.slne.surf.chat.api.entity.User
-import org.gradle.internal.impldep.jcifs.dcerpc.UUID
+import dev.slne.surf.surfapi.core.api.util.requiredService
+import java.util.UUID
 
 interface UserService {
     fun getUser(uuid: UUID): User?
     fun getUser(name: String): User?
+
+    companion object {
+        val INSTANCE = requiredService<UserService>()
+    }
 }
+
+val userService get() = UserService.INSTANCE

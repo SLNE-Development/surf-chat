7b7460f180b2cb2b6c11853bf056a20f48643329
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-21 16:30:54 +0200

    feat: register AsyncChatListener in BukkitListenerManager for chat event handling

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/BukkitListenerManager.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/BukkitListenerManager.kt
@@ -1,8 +1,11 @@
 package dev.slne.surf.chat.bukkit
 
+import dev.slne.surf.chat.bukkit.listener.AsyncChatListener
+import dev.slne.surf.surfapi.bukkit.api.event.register
+
 object BukkitListenerManager {
     fun registerBukkitListeners() {
-
+        AsyncChatListener().register()
     }
 
     fun registerPacketListeners() {

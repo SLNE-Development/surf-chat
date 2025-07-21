c2ae64e372493dee4bc9978e6af796e56bddace0
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-21 16:30:49 +0200

    feat: add CompletedComponents class for chat message interactions and user permissions

---
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/util/components.kt
@@ -0,0 +1,115 @@
+package dev.slne.surf.chat.bukkit.util
+
+import dev.slne.surf.chat.api.entity.User
+import dev.slne.surf.chat.api.model.Channel
+import dev.slne.surf.chat.bukkit.hook.PlaceholderAPIHook
+import dev.slne.surf.chat.bukkit.permission.SurfChatPermissionRegistry
+import dev.slne.surf.chat.core.message.MessageData
+import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.event.ClickEvent
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.entity.Player
+import java.util.*
+
+class CompletedComponents {
+    fun delete(messageID: UUID, viewer: User): Component {
+        if (!viewer.hasPermission(SurfChatPermissionRegistry.COMMAND_SURFCHAT_DELETE)) {
+            return Component.empty()
+        }
+
+        return buildText {
+            darkSpacer("[")
+            error("X")
+            darkSpacer("]")
+            darkSpacer(" ")
+            clickEvent(ClickEvent.runCommand("/surfchat delete $messageID"))
+            hoverEvent(buildText {
+                warning("Klicke, um die Nachricht zu löschen")
+            })
+        }
+    }
+
+    fun messageHover(messageData: MessageData) = buildText {
+        info("Gesendet von ")
+        variableValue(messageData.sender.name)
+        info(" um ")
+        variableValue(messageData.sentAt.unixTime())
+    }
+
+    fun name(player: Player) = buildText {
+        append(
+            MiniMessage.miniMessage()
+                .deserialize(
+                    convertLegacy(
+                        PlaceholderAPIHook.parse(
+                            player,
+                            "%luckperms_prefix% %player_name%"
+                        )
+                    )
+                )
+        )
+    }
+
+    fun teleport(name: String, viewer: User): Component {
+        if (!viewer.hasPermission(SurfChatPermissionRegistry.COMMAND_SURFCHAT_TELEPORT)) {
+            return Component.empty()
+        }
+
+        return buildText {
+            darkSpacer("[")
+            info("TP")
+            darkSpacer("]")
+            darkSpacer(" ")
+            clickEvent(ClickEvent.runCommand("/tp $name"))
+            hoverEvent(buildText {
+                info("Klicke, um dich zu $name zu teleportieren")
+            })
+        }
+    }
+
+    fun channelTransferConfirm(target: String): Component = buildText {
+        darkSpacer("[")
+        success("BESTÄTIGEN")
+        darkSpacer("]")
+        darkSpacer(" ")
+        clickEvent(ClickEvent.runCommand("/channel transfer $target confirm"))
+        hoverEvent(buildText {
+            success("Klicke, um die Aktion zu bestätigen")
+        })
+    }
+
+    fun channel(channel: String): Component = buildText {
+        darkSpacer("[")
+        variableValue(channel)
+        darkSpacer("]")
+        darkSpacer(" ")
+    }
+
+    fun inviteAccept(channel: Channel): Component = buildText {
+        darkSpacer("[")
+        success("AKZEPTIEREN")
+        darkSpacer("] ")
+        clickEvent(ClickEvent.runCommand("/channel accept ${channel.channelName}"))
+        hoverEvent(buildText {
+            success("Klicke, um die Einladung zu ${channel.channelName} anzunehmen")
+        })
+    }
+
+    fun inviteDecline(channel: Channel): Component = buildText {
+        darkSpacer("[")
+        error("ABLEHNEN")
+        darkSpacer("] ")
+        clickEvent(ClickEvent.runCommand("/channel decline ${channel.channelName}"))
+        hoverEvent(buildText {
+            error("Klicke, um die Einladung zu ${channel.channelName} abzulehnen")
+        })
+    }
+
+
+    companion object {
+        val INSTANCE = CompletedComponents()
+    }
+}
+
+val components get() = CompletedComponents.INSTANCE

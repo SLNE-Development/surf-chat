8e8fe71bcf48313653d2c798b5d6ea16c6574209
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-21 16:31:01 +0200

    feat: enhance MessageData and MessageFormatter for improved chat interactions and permissions

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/util/util.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/util/util.kt
@@ -3,6 +3,24 @@ package dev.slne.surf.chat.bukkit.util
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.event.Cancellable
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+private val zone = ZoneId.of("Europe/Berlin")
+val timeFormatter: DateTimeFormatter = DateTimeFormatter
+    .ofPattern("dd.MM.yyyy, HH:mm:ss", Locale.GERMANY)
+    .withZone(zone)
+
+fun Long.unixTime(): String =
+    ZonedDateTime.ofInstant(Instant.ofEpochMilli(this), zone).format(timeFormatter)
+
+private val hexRegex = Regex("&#[A-Fa-f0-9]{6}")
+fun convertLegacy(input: String) = hexRegex.replace(input) {
+    "<#${it.value.removePrefix("&#")}>"
+}
 
 fun Cancellable.cancel() {
     isCancelled = true
--- surf-chat-core/src/main/kotlin/dev/slne/surf/chat/core/message/MessageData.kt
+++ surf-chat-core/src/main/kotlin/dev/slne/surf/chat/core/message/MessageData.kt
@@ -2,9 +2,11 @@ package dev.slne.surf.chat.core.message
 
 import dev.slne.surf.chat.api.entity.User
 import net.kyori.adventure.text.Component
+import java.util.*
 
 interface MessageData {
     val message: Component
+    val messageId: UUID
     val sender: User
     val receiver: User?
     val sentAt: Long

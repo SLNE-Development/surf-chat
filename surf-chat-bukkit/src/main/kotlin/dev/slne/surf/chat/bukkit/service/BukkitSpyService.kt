1b9b97edb442c9d89308b72dc12b5072555a421b
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-21 14:38:12 +0200

    feat: update spy service to use UUIDs instead of Player objects for improved performance and consistency

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/service/BukkitSpyService.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/service/BukkitSpyService.kt
@@ -6,75 +6,75 @@ import dev.slne.surf.chat.core.service.SpyService
 import dev.slne.surf.surfapi.core.api.util.mutableObjectListOf
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
-import it.unimi.dsi.fastutil.objects.ObjectArrayList
 import it.unimi.dsi.fastutil.objects.ObjectList
 import net.kyori.adventure.util.Services
 import org.bukkit.entity.Player
+import java.util.UUID
 
 @AutoService(SpyService::class)
 class BukkitSpyService : SpyService, Services.Fallback {
-    val channelsSpys = Object2ObjectOpenHashMap<ChannelModel, ObjectList<Player>>()
-    val privateMessageSpys = Object2ObjectOpenHashMap<Player, ObjectList<Player>>()
+    val channelsSpies = Object2ObjectOpenHashMap<ChannelModel, ObjectList<UUID>>()
+    val privateMessageSpies = Object2ObjectOpenHashMap<UUID, ObjectList<UUID>>()
 
-    override fun getChannelSpys(channel: ChannelModel): ObjectList<Player> {
-        return channelsSpys.getOrDefault(channel, mutableObjectListOf())
+    override fun getChannelSpies(channel: ChannelModel): ObjectList<UUID> {
+        return channelsSpies.getOrDefault(channel, mutableObjectListOf())
     }
 
-    override fun getPrivateMessageSpys(player: Player): ObjectList<Player> {
-        return privateMessageSpys.getOrDefault(player, mutableObjectListOf())
+    override fun getPrivateMessageSpies(player: UUID): ObjectList<UUID> {
+        return privateMessageSpies.getOrDefault(player, mutableObjectListOf())
     }
 
-    override fun addChannelSpy(player: Player, channel: ChannelModel) {
-        channelsSpys.computeIfAbsent(channel) { mutableObjectListOf() }.add(player)
+    override fun addChannelSpy(player: UUID, channel: ChannelModel) {
+        channelsSpies.computeIfAbsent(channel) { mutableObjectListOf() }.add(player)
     }
 
     override fun removeChannelSpy(
-        player: Player,
+        player: UUID,
         channel: ChannelModel
     ) {
-        if (channelsSpys.containsKey(channel)) {
-            channelsSpys[channel]?.remove(player)
+        if (channelsSpies.containsKey(channel)) {
+            channelsSpies[channel]?.remove(player)
         }
     }
 
-    override fun addPrivateMessageSpy(player: Player, target: Player) {
-        privateMessageSpys.computeIfAbsent(target) { mutableObjectListOf() }.add(player)
+    override fun addPrivateMessageSpy(player: UUID, target: UUID) {
+        privateMessageSpies.computeIfAbsent(target) { mutableObjectListOf() }.add(player)
     }
 
-    override fun removePrivateMessageSpy(player: Player, target: Player) {
-        if (privateMessageSpys.containsKey(target)) {
-            privateMessageSpys[target]?.remove(player)
+    override fun removePrivateMessageSpy(player: UUID, target: UUID) {
+        if (privateMessageSpies.containsKey(target)) {
+            privateMessageSpies[target]?.remove(player)
         }
     }
 
     override fun hasChannelSpies(channel: ChannelModel): Boolean {
-        return channelsSpys.containsKey(channel) && channelsSpys[channel]?.isNotEmpty() == true
+        return channelsSpies.containsKey(channel) && channelsSpies[channel]?.isNotEmpty() == true
     }
 
-    override fun hasPrivateMessageSpies(player: Player): Boolean {
-        return privateMessageSpys.containsKey(player) && privateMessageSpys[player]?.isNotEmpty() == true
+    override fun hasPrivateMessageSpies(player: UUID): Boolean {
+        return privateMessageSpies.containsKey(player) && privateMessageSpies[player]?.isNotEmpty() == true
     }
 
-    override fun isChannelSpying(player: Player): Boolean {
-        return channelsSpys.values.any { it.contains(player) }
+    override fun isChannelSpying(player: UUID): Boolean {
+        return channelsSpies.values.any { it.contains(player) }
     }
 
-    override fun isPrivateMessageSpying(player: Player): Boolean {
-        return privateMessageSpys.containsKey(player) && privateMessageSpys[player]?.isNotEmpty() == true
+    override fun isPrivateMessageSpying(player: UUID): Boolean {
+        return privateMessageSpies.containsKey(player) && privateMessageSpies[player]?.isNotEmpty() == true
     }
 
-    override fun clearChannelSpys(player: Player) {
-        channelsSpys.values.forEach { it.remove(player) }
-        channelsSpys.keys.removeIf { channelsSpys[it]?.isEmpty() == true }
+    override fun clearChannelSpies(player: UUID) {
+        channelsSpies.values.forEach { it.remove(player) }
+        channelsSpies.keys.removeIf { channelsSpies[it]?.isEmpty() == true }
     }
 
-    override fun clearPrivateMessageSpys(player: Player) {
-        privateMessageSpys.values.forEach { it.remove(player) }
-        privateMessageSpys.keys.removeIf { privateMessageSpys[it]?.isEmpty() == true }
+    override fun clearPrivateMessageSpies(player: UUID) {
+        privateMessageSpies.values.forEach { it.remove(player) }
+        privateMessageSpies.keys.removeIf { privateMessageSpies[it]?.isEmpty() == true }
     }
 
-    override fun handleDisconnect(player: Player) {
-        this.clearChannelSpys(player)
-        this.clearPrivateMessageSpys(player)
+    override fun handleDisconnect(player: UUID) {
+        this.clearChannelSpies(player)
+        this.clearPrivateMessageSpies(player)
     }
 }
--- surf-chat-core/src/main/kotlin/dev/slne/surf/chat/core/service/SpyService.kt
+++ surf-chat-core/src/main/kotlin/dev/slne/surf/chat/core/service/SpyService.kt
@@ -4,27 +4,28 @@ import dev.slne.surf.chat.api.model.ChannelModel
 import dev.slne.surf.surfapi.core.api.util.requiredService
 import it.unimi.dsi.fastutil.objects.ObjectList
 import org.bukkit.entity.Player
+import java.util.UUID
 
 interface SpyService {
-    fun getChannelSpys(channel: ChannelModel): ObjectList<Player>
-    fun getPrivateMessageSpys(player: Player): ObjectList<Player>
+    fun getChannelSpies(channel: ChannelModel): ObjectList<UUID>
+    fun getPrivateMessageSpies(player: UUID): ObjectList<UUID>
 
-    fun addChannelSpy(player: Player, channel: ChannelModel)
-    fun removeChannelSpy(player: Player, channel: ChannelModel)
+    fun addChannelSpy(player: UUID, channel: ChannelModel)
+    fun removeChannelSpy(player: UUID, channel: ChannelModel)
 
-    fun addPrivateMessageSpy(player: Player, target: Player)
-    fun removePrivateMessageSpy(player: Player, target: Player)
+    fun addPrivateMessageSpy(player: UUID, target: UUID)
+    fun removePrivateMessageSpy(player: UUID, target: UUID)
 
     fun hasChannelSpies(channel: ChannelModel): Boolean
-    fun hasPrivateMessageSpies(player: Player): Boolean
+    fun hasPrivateMessageSpies(player: UUID): Boolean
 
-    fun isChannelSpying(player: Player): Boolean
-    fun isPrivateMessageSpying(player: Player): Boolean
+    fun isChannelSpying(player: UUID): Boolean
+    fun isPrivateMessageSpying(player: UUID): Boolean
 
-    fun clearChannelSpys(player: Player)
-    fun clearPrivateMessageSpys(player: Player)
+    fun clearChannelSpies(player: UUID)
+    fun clearPrivateMessageSpies(player: UUID)
 
-    fun handleDisconnect(player: Player)
+    fun handleDisconnect(player: UUID)
 
     companion object {
         val INSTANCE = requiredService<SpyService>()

fcea05295f0719eea5e1f48174a9e5a1428558e1
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-24 00:13:20 +0200

    feat: add custom argument classes for channel management in commands

---
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/argument/ChannelInviteArgument.kt
@@ -0,0 +1,49 @@
+package dev.slne.surf.chat.bukkit.command.argument
+
+import dev.jorel.commandapi.arguments.ArgumentSuggestions
+import dev.jorel.commandapi.arguments.CustomArgument
+import dev.jorel.commandapi.arguments.StringArgument
+import dev.slne.surf.chat.api.model.Channel
+import dev.slne.surf.chat.bukkit.util.user
+import dev.slne.surf.chat.core.service.channelService
+import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
+
+class ChannelInviteArgument(nodeName: String) :
+    CustomArgument<Channel, String>(StringArgument(nodeName), { info ->
+        val channel = channelService.getChannel(info.input())
+            ?: throw CustomArgumentException.fromAdventureComponent {
+                buildText {
+                    appendPrefix()
+                    error("Der Kanal existiert nicht.")
+                }
+            }
+
+        val user = info.sender.user() ?: throw CustomArgumentException.fromAdventureComponent {
+            buildText {
+                appendPrefix()
+                error("Ein Fehler ist aufgetreten.")
+            }
+        }
+
+        if (!channel.isInvited(user)) {
+            throw CustomArgumentException.fromAdventureComponent {
+                buildText {
+                    appendPrefix()
+                    error("Du bist nicht in diesen Kanal eingeladen.")
+                }
+            }
+        }
+        channel
+    }) {
+
+    init {
+        replaceSuggestions(ArgumentSuggestions.stringCollection { info ->
+            val user = info.sender().user() ?: return@stringCollection emptyList()
+
+            channelService.getChannels()
+                .filter { it.isInvited(user) }
+                .map { it.channelName }
+        }
+        )
+    }
+}
---
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/argument/ChannelMembersArgument.kt
@@ -0,0 +1,61 @@
+package dev.slne.surf.chat.bukkit.command.argument
+
+import dev.jorel.commandapi.arguments.ArgumentSuggestions
+import dev.jorel.commandapi.arguments.CustomArgument
+import dev.jorel.commandapi.arguments.StringArgument
+import dev.slne.surf.chat.api.entity.ChannelMember
+import dev.slne.surf.chat.bukkit.util.user
+import dev.slne.surf.chat.core.service.channelService
+import dev.slne.surf.chat.core.service.userService
+import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
+import dev.slne.surf.surfapi.core.api.util.emptyObjectSet
+
+class ChannelMembersArgument(nodeName: String) :
+    CustomArgument<ChannelMember, String>(StringArgument(nodeName), { info ->
+        val target =
+            userService.getUser(info.input) ?: throw CustomArgumentException.fromMessageBuilder(
+                MessageBuilder("Der Spieler ${info.input} wurde nicht gefunden.")
+            )
+        val user = info.sender.user() ?: throw CustomArgumentException.fromAdventureComponent {
+            buildText {
+                appendPrefix()
+                error("Ein Fehler ist aufgetreten.")
+            }
+        }
+
+        val channel = channelService.getChannel(user)
+            ?: throw CustomArgumentException.fromAdventureComponent {
+                buildText {
+                    appendPrefix()
+                    error("Du bist in keinem Nachrichtenkanal.")
+                }
+            }
+
+        if (!channel.isMember(target)) {
+            throw CustomArgumentException.fromAdventureComponent {
+                buildText {
+                    appendPrefix()
+                    error("Der Spieler ${target.name} ist kein Mitglied in deinem Nachrichtenkanal.")
+                }
+            }
+        }
+
+        target.channelMember(channel)
+    }) {
+    init {
+        this.replaceSuggestions(ArgumentSuggestions.stringCollection { info ->
+            val user = info.sender.user() ?: return@stringCollection emptyObjectSet()
+            val channel =
+                channelService.getChannel(user) ?: return@stringCollection emptyObjectSet()
+            val members = channel.members
+
+            if (members.isEmpty()) {
+                return@stringCollection emptyObjectSet()
+            }
+
+            return@stringCollection members.map {
+                it.name
+            }
+        })
+    }
+}

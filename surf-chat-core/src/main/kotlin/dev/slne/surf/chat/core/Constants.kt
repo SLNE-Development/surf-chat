56d6cc919f2592b1c3d37e63530a16e83848c245
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-25 19:11:09 +0200

    feat: add CHANNEL_CHAT constant and implement plugin message sender in AsyncChatListener

--- surf-chat-core/src/main/kotlin/dev/slne/surf/chat/core/Constants.kt
+++ surf-chat-core/src/main/kotlin/dev/slne/surf/chat/core/Constants.kt
@@ -3,4 +3,5 @@ package dev.slne.surf.chat.core
 object Constants {
     const val TEAM_PERMISSION = "surf.chat.team"
     const val CHANNEL_TEAM = "surf-chat:teamchat"
+    const val CHANNEL_CHAT = "surf-chat:chat-sync"
 }

8f478e08277aca06fec85bc001545a998b313c48
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-28 00:24:52 +0200

    feat: add command to force delete channels with permission validation and user feedback

---
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelForceDeleteCommand.kt
@@ -0,0 +1,27 @@
+package dev.slne.surf.chat.bukkit.command.channel
+
+import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.kotlindsl.getValue
+import dev.jorel.commandapi.kotlindsl.playerExecutor
+import dev.jorel.commandapi.kotlindsl.subcommand
+import dev.slne.surf.chat.api.model.Channel
+import dev.slne.surf.chat.bukkit.command.argument.channelArgument
+import dev.slne.surf.chat.bukkit.permission.SurfChatPermissionRegistry
+import dev.slne.surf.chat.core.service.channelService
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+
+fun CommandAPICommand.channelForceDeleteCommand() = subcommand("delete") {
+    withPermission(SurfChatPermissionRegistry.COMMAND_CHANNEL_ADMIN_DELETE)
+    channelArgument("channel")
+    playerExecutor { player, args ->
+        val channel: Channel by args
+
+        channelService.deleteChannel(channel)
+
+        player.sendText {
+            success("Der Nachrichtenkanal ")
+            variableValue(channel.channelName)
+            success(" wurde gelöscht.")
+        }
+    }
+}

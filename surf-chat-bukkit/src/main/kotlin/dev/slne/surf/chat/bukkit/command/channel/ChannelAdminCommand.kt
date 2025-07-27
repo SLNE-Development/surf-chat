ff74aa840867cc412cf5d7b3c4facc5dccae394b
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-28 00:41:05 +0200

    feat: refactor channel admin and info commands for improved structure and functionality

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelAdminCommand.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelAdminCommand.kt
@@ -7,8 +7,9 @@ import dev.slne.surf.chat.bukkit.permission.SurfChatPermissionRegistry
 fun channelAdminCommand() = commandAPICommand("channeladmin") {
     withPermission(SurfChatPermissionRegistry.COMMAND_CHANNEL_ADMIN)
 
+    channelForceJoinCommand()
+    channelForceDeleteCommand()
+
     subcommand(ChannelMoveCommand("move"))
-    subcommand(ChannelForceDeleteCommand("forceDelete"))
-    subcommand(ChannelForceJoinCommand("forceJoin"))
     subcommand(ChannelSpyCommand("spy"))
 }
--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelInfoCommand.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelInfoCommand.kt
@@ -1,67 +1,96 @@
 package dev.slne.surf.chat.bukkit.command.channel
 
 import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.kotlindsl.getValue
 import dev.jorel.commandapi.kotlindsl.playerExecutor
-import dev.slne.surf.chat.api.model.ChannelModel
-import dev.slne.surf.chat.api.type.ChannelStatusType
-import dev.slne.surf.chat.bukkit.command.argument.ChannelArgument
-import dev.slne.surf.chat.bukkit.util.ChatPermissionRegistry
-import dev.slne.surf.chat.bukkit.util.utils.sendPrefixed
+import dev.jorel.commandapi.kotlindsl.subcommand
+import dev.slne.surf.chat.api.model.Channel
+import dev.slne.surf.chat.api.model.ChannelRole
+import dev.slne.surf.chat.api.model.ChannelVisibility
+import dev.slne.surf.chat.bukkit.command.argument.channelArgument
+import dev.slne.surf.chat.bukkit.permission.SurfChatPermissionRegistry
+import dev.slne.surf.chat.bukkit.util.user
 import dev.slne.surf.chat.core.service.channelService
 import dev.slne.surf.surfapi.core.api.font.toSmallCaps
 import dev.slne.surf.surfapi.core.api.messages.Colors
 import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
-import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
-import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextDecoration
 
-class ChannelInfoCommand(commandName: String) : CommandAPICommand(commandName) {
-    init {
-        withPermission(ChatPermissionRegistry.COMMAND_CHANNEL_INFO)
-        withOptionalArguments(ChannelArgument("channel"))
-        playerExecutor { player, args ->
-            val channel = args.getOrDefaultUnchecked<ChannelModel?>(
-                "channel",
-                channelService.getChannel(player)
-            ) ?: run {
-                player.sendPrefixed {
-                    error("Der Kanal existiert nicht oder ist nicht für dich zugänglich.")
-                }
+fun CommandAPICommand.channelInfoCommand() = subcommand("info") {
+    withPermission(SurfChatPermissionRegistry.COMMAND_CHANNEL_INFO)
+    channelArgument("channel", true)
+    playerExecutor { player, args ->
+        val user = player.user() ?: return@playerExecutor
+        val channel: Channel? by args
+
+        if (channel == null) {
+            val userChannel = channelService.getChannel(user) ?: run {
+                player.sendMessage(
+                    buildText {
+                        appendPrefix()
+                        error("Du bist in keinem Nachrichtenkanal.")
+                    }
+                )
                 return@playerExecutor
             }
 
-            player.sendMessage(createInfoMessage(channel))
+            player.sendMessage(createInfoMessage(userChannel))
+            return@playerExecutor
         }
-    }
 
-    private fun createInfoMessage(channel: ChannelModel): Component {
-        return buildText {
-            info("Informationen".toSmallCaps()).appendNewline()
-            append {
-                info("| ")
-                decorate(TextDecoration.BOLD)
-            }.spacer("Name: ".toSmallCaps()).text(channel.name, Colors.WHITE).appendNewline()
-            append {
-                info("| ")
-                decorate(TextDecoration.BOLD)
-            }.spacer("Besitzer: ".toSmallCaps()).text(channel.getOwner().getName(), Colors.WHITE).appendNewline()
-            append {
-                info("| ")
-                decorate(TextDecoration.BOLD)
-            }.spacer("Modus: ".toSmallCaps()).text(
-                when (channel.status) {
-                    ChannelStatusType.PUBLIC -> "Öffentlich"
-                    ChannelStatusType.PRIVATE -> "Privat"
-                }, Colors.WHITE
-            ).appendNewline()
-            append {
-                info("| ")
-                decorate(TextDecoration.BOLD)
-            }.spacer("Mitglieder: ".toSmallCaps()).text(channel.members.size, Colors.WHITE).appendNewline()
-            append {
-                info("| ")
-                decorate(TextDecoration.BOLD)
-            }.spacer("Einladungen: ".toSmallCaps()).text(channel.invites.size, Colors.WHITE)
-        }
+        player.sendMessage(createInfoMessage(channel ?: run {
+            player.sendMessage(
+                buildText {
+                    appendPrefix()
+                    error("Der Nachrichtenkanal wurde nicht gefunden.")
+                }
+            )
+            return@playerExecutor
+        }))
     }
+}
+
+private fun createInfoMessage(channel: Channel) = buildText {
+    info("Informationen".toSmallCaps()).appendNewline()
+    append {
+        info("| ")
+        decorate(TextDecoration.BOLD)
+    }
+    spacer("Name: ".toSmallCaps())
+    text(channel.channelName, Colors.WHITE).appendNewline()
+    append {
+        info("| ")
+        decorate(TextDecoration.BOLD)
+    }
+    spacer("Besitzer: ".toSmallCaps())
+    text(
+        channel.members.firstOrNull { it.role == ChannelRole.OWNER }?.name ?: "Error",
+        Colors.WHITE
+    )
+    appendNewline()
+    append {
+        info("| ")
+        decorate(TextDecoration.BOLD)
+    }
+    spacer("Modus: ".toSmallCaps())
+    text(
+        when (channel.visibility) {
+            ChannelVisibility.PUBLIC -> "Öffentlich"
+            ChannelVisibility.PRIVATE -> "Privat"
+        }, Colors.WHITE
+    )
+    appendNewline()
+    append {
+        info("| ")
+        decorate(TextDecoration.BOLD)
+    }
+    spacer("Mitglieder: ".toSmallCaps())
+    text(channel.members.size, Colors.WHITE)
+    appendNewline()
+    append {
+        info("| ")
+        decorate(TextDecoration.BOLD)
+    }
+    spacer("Einladungen: ".toSmallCaps())
+    text(channel.invitedPlayers.size, Colors.WHITE)
 }

6f7e5d3bc9c68e1aeb8e417c527d02befbc76157
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-28 00:24:56 +0200

    feat: add command to force join channels with permission validation and user feedback

---
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelForceJoinCommand.kt
@@ -0,0 +1,36 @@
+package dev.slne.surf.chat.bukkit.command.channel
+
+import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.kotlindsl.getValue
+import dev.jorel.commandapi.kotlindsl.playerExecutor
+import dev.jorel.commandapi.kotlindsl.subcommand
+import dev.slne.surf.chat.api.model.Channel
+import dev.slne.surf.chat.bukkit.command.argument.channelArgument
+import dev.slne.surf.chat.bukkit.permission.SurfChatPermissionRegistry
+import dev.slne.surf.chat.bukkit.util.user
+import dev.slne.surf.chat.core.service.channelService
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+
+fun CommandAPICommand.channelForceJoinCommand() = subcommand("join") {
+    withPermission(SurfChatPermissionRegistry.COMMAND_CHANNEL_ADMIN_JOIN)
+    channelArgument("channel")
+    playerExecutor { player, args ->
+        val channel: Channel by args
+        val user = player.user() ?: return@playerExecutor
+
+        if (channelService.getChannel(user) != null) {
+            player.sendText {
+                error("Du bist bereits in einem Nachrichtenkanal.")
+            }
+            return@playerExecutor
+        }
+
+        channel.addMember(user)
+
+        player.sendText {
+            success("Du bist dem Nachrichtenkanal ")
+            variableValue(channel.channelName)
+            success(" beigetreten.")
+        }
+    }
+}

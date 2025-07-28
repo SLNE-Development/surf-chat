c5cc68436421e97cbb2e55fda9e98b2888e9e671
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-28 13:41:34 +0200

    feat: refactor ChannelTransferCommand to use subcommand structure and improve user feedback

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelTransferCommand.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelTransferCommand.kt
@@ -1,100 +1,104 @@
 package dev.slne.surf.chat.bukkit.command.channel
 
-import com.github.shynixn.mccoroutine.folia.launch
-
 import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.kotlindsl.getValue
 import dev.jorel.commandapi.kotlindsl.playerExecutor
-
-import dev.slne.surf.chat.api.model.ChannelModel
-import dev.slne.surf.chat.api.type.ChannelRoleType
-import dev.slne.surf.chat.bukkit.command.argument.ChannelMembersArgument
-import dev.slne.surf.chat.bukkit.plugin
-import dev.slne.surf.chat.bukkit.util.ChatPermissionRegistry
-import dev.slne.surf.chat.bukkit.util.utils.sendPrefixed
+import dev.jorel.commandapi.kotlindsl.subcommand
+import dev.slne.surf.chat.api.model.Channel
+import dev.slne.surf.chat.api.model.ChannelRole
+import dev.slne.surf.chat.bukkit.command.argument.channelMembersArgument
+import dev.slne.surf.chat.bukkit.permission.SurfChatPermissionRegistry
+import dev.slne.surf.chat.bukkit.util.sendText
+import dev.slne.surf.chat.bukkit.util.user
 import dev.slne.surf.chat.core.service.channelService
-import dev.slne.surf.chat.core.service.databaseService
-import dev.slne.surf.surfapi.core.api.messages.Colors
 import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
-
 import net.kyori.adventure.text.event.ClickEvent
-import net.kyori.adventure.text.Component
+import org.bukkit.entity.Player
 
-import org.bukkit.OfflinePlayer
-
-class ChannelTransferCommand(commandName: String) : CommandAPICommand(commandName) {
-    init {
-        withPermission(ChatPermissionRegistry.COMMAND_CHANNEL_TRANSFER)
-        withArguments(ChannelMembersArgument("member"))
-
-        playerExecutor { player, args ->
-            val channel: ChannelModel? = channelService.getChannel(player)
-            val target = args.getUnchecked<OfflinePlayer>("member") ?: return@playerExecutor
-
-            plugin.launch {
-                val user = databaseService.getUser(player.uniqueId)
-                val targetUser = databaseService.getUser(target.uniqueId)
-
-                if (channel == null) {
-                    user.sendPrefixed{
-                        error("Du bist in keinem Nachrichtenkanal.")
-                    }
-                    return@launch
-                }
-
-                if (!channel.isOwner(user)) {
-                    user.sendPrefixed{
-                        error("Du verfügst nicht über die erforderliche Berechtigung.")
-                    }
-                    return@launch
-                }
-
-                if (!channel.isMember(targetUser)) {
-                    user.sendPrefixed{
-                        error("Der Spieler ")
-                        variableValue(target.name ?: target.uniqueId.toString())
-                        error(" ist kein Mitglied in deinem Nachrichtenkanal.")
-                    }
-                    return@launch
-                }
-
-                val confirmComponent: Component = buildText {
-                    info("Klicke hier, um den Vorgang zu bestätigen. ")
-                    success("[BESTÄTIGEN]")
-                    hoverEvent(buildText {
-                        info("Klicke hier, um den Vorgang zu bestätigen.")
-                    })
-                    clickEvent(ClickEvent.callback {
-                        plugin.launch {
-                            if (channel.members.none { it.value == ChannelRoleType.OWNER }) {
-                                user.sendPrefixed {
-                                    error("Der Nachrichtenkanal benötigt mindestens einen Besitzer.")
-                                }
-                                return@launch
-                            }
-
-                            channel.transferOwnership(targetUser)
-
-                            user.sendPrefixed {
-                                success("Du hast den Nachrichtenkanal ")
-                                variableValue(channel.name)
-                                success(" an ")
-                                variableValue(target.name ?: target.uniqueId.toString())
-                                success(" übertragen.")
-                            }
-
-                            targetUser.sendPrefixed {
-                                info("Du bist jetzt der Besitzer des Nachrichtenkanals ")
-                                variableValue(channel.name)
-                                info(".")
-                            }
-                        }
-                    })
-                }
-
-                user.sendPrefixed {
-                    append(confirmComponent)
-                }
+fun CommandAPICommand.channelTransferCommand() = subcommand("transfer") {
+    withPermission(SurfChatPermissionRegistry.COMMAND_CHANNEL_TRANSFER)
+    channelMembersArgument("member")
+    playerExecutor { player, args ->
+        val user = player.user() ?: return@playerExecutor
+        val channel: Channel = channelService.getChannel(user) ?: run {
+            user.sendText {
+                appendPrefix()
+                error("Du bist in keinem Nachrichtenkanal.")
             }
+            return@playerExecutor
+        }
+        val target: Player by args
+
+        if (!channel.isOwner(user)) {
+            user.sendText {
+                appendPrefix()
+                error("Du verfügst nicht über die erforderliche Berechtigung.")
+            }
+            return@playerExecutor
+        }
+
+        val targetUser = target.user() ?: run {
+            user.sendText {
+                appendPrefix()
+                error("Der Spieler ist nicht online oder existiert nicht.")
+            }
+            return@playerExecutor
+        }
+
+        if (!channel.isMember(targetUser)) {
+            user.sendText {
+                appendPrefix()
+                error("Der Spieler ")
+                variableValue(target.name)
+                error(" ist kein Mitglied in deinem Nachrichtenkanal.")
+            }
+            return@playerExecutor
+        }
+
+        user.sendText {
+            appendPrefix()
+            info("Klicke hier, um den Vorgang zu bestätigen. ")
+            success("[BESTÄTIGEN]")
+            hoverEvent(buildText {
+                info("Klicke hier, um den Vorgang zu bestätigen.")
+            })
+            clickEvent(ClickEvent.callback {
+                if (channel.members.none { it.role == ChannelRole.OWNER }) {
+                    user.sendText {
+                        appendPrefix()
+                        error("Der Nachrichtenkanal benötigt mindestens einen Besitzer.")
+                    }
+                    return@callback
+                }
+
+                val targetMember = targetUser.channelMember(channel) ?: run {
+                    user.sendText {
+                        appendPrefix()
+                        error("Der Spieler ")
+                        variableValue(target.name)
+                        error(" ist nicht im Nachrichtenkanal.")
+                    }
+                    return@callback
+                }
+
+                channel.transfer(targetMember)
+
+                user.sendText {
+                    appendPrefix()
+                    success("Du hast den Nachrichtenkanal ")
+                    variableValue(channel.channelName)
+                    success(" an ")
+                    variableValue(target.name)
+                    success(" übertragen.")
+                }
+
+                targetUser.sendText {
+                    appendPrefix()
+                    info("Du bist jetzt der Besitzer des Nachrichtenkanals ")
+                    variableValue(channel.channelName)
+                    info(".")
+                }
+            })
         }
     }
 }

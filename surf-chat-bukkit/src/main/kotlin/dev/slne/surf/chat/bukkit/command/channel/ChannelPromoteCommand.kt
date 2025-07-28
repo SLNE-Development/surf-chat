393834c2b7deda63501abc5b43f173091534e13a
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-28 12:06:01 +0200

    feat: refactor ChannelPromoteCommand to use subcommand structure and improve user feedback

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelPromoteCommand.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelPromoteCommand.kt
@@ -1,66 +1,72 @@
 package dev.slne.surf.chat.bukkit.command.channel
 
-import com.github.shynixn.mccoroutine.folia.launch
 import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.kotlindsl.getValue
 import dev.jorel.commandapi.kotlindsl.playerExecutor
-import dev.slne.surf.chat.api.model.ChannelModel
-import dev.slne.surf.chat.api.surfChatApi
-import dev.slne.surf.chat.bukkit.command.argument.ChannelMembersArgument
-import dev.slne.surf.chat.bukkit.plugin
-import dev.slne.surf.chat.bukkit.util.ChatPermissionRegistry
-import dev.slne.surf.chat.bukkit.util.utils.sendPrefixed
+import dev.jorel.commandapi.kotlindsl.subcommand
+import dev.slne.surf.chat.api.model.Channel
+import dev.slne.surf.chat.bukkit.command.argument.channelMembersArgument
+import dev.slne.surf.chat.bukkit.permission.SurfChatPermissionRegistry
+import dev.slne.surf.chat.bukkit.util.sendText
+import dev.slne.surf.chat.bukkit.util.user
 import dev.slne.surf.chat.core.service.channelService
-import dev.slne.surf.chat.core.service.databaseService
-import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
-import org.bukkit.OfflinePlayer
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import org.bukkit.entity.Player
 
-class ChannelPromoteCommand(commandName: String) : CommandAPICommand(commandName) {
-    init {
-        withPermission(ChatPermissionRegistry.COMMAND_CHANNEL_PROMOTE)
-        withArguments(ChannelMembersArgument("player"))
-        playerExecutor { player, args ->
-            val channel: ChannelModel? = channelService.getChannel(player)
-            val target = args.getUnchecked<OfflinePlayer>("player") ?: return@playerExecutor
-
-            if(channel == null) {
-                player.sendPrefixed {
-                    error("Du bist in keinem Nachrichtenkanal.")
-                }
-                return@playerExecutor
+fun CommandAPICommand.channelPromoteCommand() = subcommand("promote") {
+    withPermission(SurfChatPermissionRegistry.COMMAND_CHANNEL_PROMOTE)
+    channelMembersArgument("target")
+    playerExecutor { player, args ->
+        val user = player.user() ?: return@playerExecutor
+        val channel: Channel = channelService.getChannel(user) ?: run {
+            player.sendText {
+                appendPrefix()
+                error("Du bist in keinem Nachrichtenkanal.")
             }
+            return@playerExecutor
+        }
+        val target: Player by args
 
-            plugin.launch {
-                val user = databaseService.getUser(player.uniqueId)
-                val targetUser = databaseService.getUser(target.uniqueId)
-
-                if (!channel.isOwner(user)) {
-                    user.sendPrefixed {
-                        error("Du verfügst nicht über die erforderliche Berechtigung.")
-                    }
-                    return@launch
-                }
-
-                if (channel.hasModeratorPermissions(targetUser)) {
-                    user.sendPrefixed {
-                        error("Der Spieler ")
-                        variableValue(target.name ?: target.uniqueId.toString())
-                        error(" ist bereits Moderator.")
-                    }
-                    return@launch
-                }
-
-                channel.promote(targetUser)
-
-                user.sendPrefixed {
-                    success("Du hast ")
-                    variableValue(target.name ?: target.uniqueId.toString())
-                    success(" befördert.")
-                }
-
-                targetUser.sendPrefixed {
-                    info("Du wurdest befördert.")
-                }
+        if (!channel.isOwner(user)) {
+            user.sendText {
+                appendPrefix()
+                error("Du verfügst nicht über die erforderliche Berechtigung.")
             }
+            return@playerExecutor
+        }
+
+        val targetMember = target.user()?.channelMember(channel) ?: run {
+            user.sendText {
+                appendPrefix()
+                error("Der Spieler ")
+                variableValue(target.name)
+                error(" ist nicht im Nachrichtenkanal.")
+            }
+            return@playerExecutor
+        }
+
+        if (targetMember.hasModeratorPermissions()) {
+            user.sendText {
+                appendPrefix()
+                error("Der Spieler ")
+                variableValue(target.name)
+                error(" ist bereits Moderator.")
+            }
+            return@playerExecutor
+        }
+
+        channel.promote(targetMember)
+
+        user.sendText {
+            appendPrefix()
+            success("Du hast ")
+            variableValue(target.name)
+            success(" befördert.")
+        }
+
+        target.sendText {
+            appendPrefix()
+            info("Du wurdest befördert.")
         }
     }
 }

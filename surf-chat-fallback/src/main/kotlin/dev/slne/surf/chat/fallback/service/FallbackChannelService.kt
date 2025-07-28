00f7a8d74589fc23906a30ea727843a3a8d5b0ec
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-28 14:15:08 +0200

    feat: implement channel creation, deletion, and member management in FallbackChannelService

--- surf-chat-fallback/src/main/kotlin/dev/slne/surf/chat/fallback/service/FallbackChannelService.kt
+++ surf-chat-fallback/src/main/kotlin/dev/slne/surf/chat/fallback/service/FallbackChannelService.kt
@@ -1,94 +1,126 @@
 package dev.slne.surf.chat.fallback.service
 
 import com.google.auto.service.AutoService
+import dev.slne.surf.chat.api.entity.ChannelMember
 import dev.slne.surf.chat.api.entity.User
 import dev.slne.surf.chat.api.model.Channel
+import dev.slne.surf.chat.api.model.ChannelRole
+import dev.slne.surf.chat.api.model.ChannelVisibility
 import dev.slne.surf.chat.core.service.ChannelService
+import dev.slne.surf.chat.fallback.entity.FallbackChannelMember
+import dev.slne.surf.chat.fallback.model.FallbackChannel
+import dev.slne.surf.surfapi.core.api.util.mutableObjectSetOf
 import it.unimi.dsi.fastutil.objects.ObjectSet
 import net.kyori.adventure.util.Services
 import java.util.*
 
 @AutoService(ChannelService::class)
 class FallbackChannelService : ChannelService, Services.Fallback {
+    val channels = mutableMapOf<UUID, Channel>()
     override fun createChannel(
         name: String,
         owner: User
     ): Channel {
-        TODO("Not yet implemented")
+        val members = mutableObjectSetOf<ChannelMember>()
+
+        members.add(
+            FallbackChannelMember(
+                owner.uuid, owner.name, ChannelRole.OWNER
+            )
+        )
+
+        val channel = FallbackChannel(
+            UUID.randomUUID(), name, members, mutableObjectSetOf(), mutableObjectSetOf(),
+            ChannelVisibility.PRIVATE, System.currentTimeMillis()
+        )
+
+        this.registerChannel(channel)
+        return channel
     }
 
     override fun deleteChannel(channel: Channel) {
-        TODO("Not yet implemented")
+        unregisterChannel(channel)
     }
 
-    override fun getChannel(name: String): Channel? {
-        TODO("Not yet implemented")
-    }
+    override fun getChannel(name: String) =
+        channels.values.firstOrNull { it.channelName == name }
 
-    override fun getChannel(channelUuid: UUID): Channel? {
-        TODO("Not yet implemented")
-    }
+    override fun getChannel(channelUuid: UUID) =
+        channels[channelUuid]
 
-    override fun getChannel(user: User): Channel? {
-        return null
-    }
+    override fun getChannel(user: User) =
+        channels.values.firstOrNull { it.isMember(user) }
 
-    override fun getChannels(): ObjectSet<Channel> {
-        TODO("Not yet implemented")
-    }
+    override fun getChannels(): ObjectSet<Channel> =
+        mutableObjectSetOf(*channels.values.toTypedArray())
 
     override fun invite(
         channel: Channel,
         user: User
-    ): Boolean {
-        TODO("Not yet implemented")
-    }
+    ) = channel.invite(user)
 
     override fun uninvite(
         channel: Channel,
         user: User
-    ): Boolean {
-        TODO("Not yet implemented")
-    }
+    ) = channel.revoke(user)
 
     override fun isInvited(
         channel: Channel,
         user: User
-    ): Boolean {
-        TODO("Not yet implemented")
-    }
+    ) = channel.isInvited(user)
 
     override fun acceptInvite(
         channel: Channel,
         user: User
     ): Boolean {
-        TODO("Not yet implemented")
+        if (!channel.isInvited(user)) {
+            return false
+        }
+
+        channel.join(user)
+        channel.revoke(user)
+        return true
     }
 
     override fun declineInvite(
         channel: Channel,
         user: User
     ): Boolean {
-        TODO("Not yet implemented")
+        if (!channel.isInvited(user)) {
+            return false
+        }
+
+        channel.revoke(user)
+        return true
     }
 
     override fun registerChannel(channel: Channel) {
-        TODO("Not yet implemented")
+        channels[channel.channelUuid] = channel
     }
 
     override fun unregisterChannel(channel: Channel) {
-        TODO("Not yet implemented")
+        channels.remove(channel.channelUuid)
     }
 
     override fun getRegisteredChannels(): ObjectSet<Channel> {
-        TODO("Not yet implemented")
+        return mutableObjectSetOf(*channels.values.toTypedArray())
     }
 
     override fun move(
         user: User,
         channel: Channel
     ): Boolean {
-        TODO("Not yet implemented")
-    }
+        val currentChannel = this.getChannel(user)
 
+        currentChannel?.let { currentChannel ->
+            val currentMember = user.channelMember(currentChannel)
+
+            currentMember?.let {
+                currentChannel.removeMember(currentMember)
+            }
+        }
+
+        channel.join(user)
+        return true
+    }
 }

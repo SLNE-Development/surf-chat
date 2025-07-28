1ff890dd96e54706bb099531ef6ffb47bcf0d484
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-28 09:45:33 +0200

    feat: refactor ChannelKickCommand for improved readability and consistency

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelKickCommand.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelKickCommand.kt
@@ -1,69 +1,75 @@
 package dev.slne.surf.chat.bukkit.command.channel
 
-import com.github.shynixn.mccoroutine.folia.launch
 import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.kotlindsl.getValue
 import dev.jorel.commandapi.kotlindsl.playerExecutor
-
-import dev.slne.surf.chat.api.model.ChannelModel
-import dev.slne.surf.chat.api.surfChatApi
-import dev.slne.surf.chat.bukkit.command.argument.ChannelMembersArgument
-import dev.slne.surf.chat.bukkit.plugin
-import dev.slne.surf.chat.bukkit.util.ChatPermissionRegistry
-import dev.slne.surf.chat.bukkit.util.utils.sendPrefixed
+import dev.jorel.commandapi.kotlindsl.subcommand
+import dev.slne.surf.chat.api.model.Channel
+import dev.slne.surf.chat.bukkit.command.argument.channelMembersArgument
+import dev.slne.surf.chat.bukkit.permission.SurfChatPermissionRegistry
+import dev.slne.surf.chat.bukkit.util.sendText
+import dev.slne.surf.chat.bukkit.util.user
 import dev.slne.surf.chat.core.service.channelService
-import dev.slne.surf.chat.core.service.databaseService
-import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
-
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
 import org.bukkit.entity.Player
 
-class ChannelKickCommand(commandName: String) : CommandAPICommand(commandName) {
-    init {
-        withPermission(ChatPermissionRegistry.COMMAND_CHANNEL_KICK)
-        withArguments(ChannelMembersArgument("player"))
-        playerExecutor { player, args ->
-            val channel: ChannelModel? = channelService.getChannel(player)
-            val target = args.getUnchecked<Player>("player") ?: return@playerExecutor
+fun CommandAPICommand.channelKickCommand() = subcommand("kick") {
+    withPermission(SurfChatPermissionRegistry.COMMAND_CHANNEL_KICK)
+    channelMembersArgument("target")
+    playerExecutor { player, args ->
+        val user = player.user() ?: return@playerExecutor
+        val channel: Channel? = channelService.getChannel(user)
+        val target: Player by args
 
-            if(channel == null) {
-                player.sendPrefixed {
+        if (channel == null) {
+            player.sendText {
+                error("Du bist in keinem Nachrichtenkanal.")
+            }
+            return@playerExecutor
+        }
+
+        val userMember = user.channelMember(channel)
+            ?: return@playerExecutor run {
+                player.sendText {
                     error("Du bist in keinem Nachrichtenkanal.")
                 }
-                return@playerExecutor
             }
 
-            plugin.launch {
-                val user = databaseService.getUser(player.uniqueId)
-                val targetUser = databaseService.getUser(target.uniqueId)
-
-                if (!channel.hasModeratorPermissions(user)) {
-                    user.sendPrefixed {
-                        error("Du verfügst nicht über die erforderliche Berechtigung.")
-                    }
-                    return@launch
-                }
-
-                if (channel.hasModeratorPermissions(targetUser) && channel.hasModeratorPermissions(user)) {
-                    user.sendPrefixed {
-                        error("Du kannst keine Spieler aus diesem Kanal entfernen.")
-                    }
-                    return@launch
-                }
-
-                channel.kick(targetUser)
-                user.sendPrefixed {
-                    info("Du hast ")
-                    variableValue(targetUser.getName())
-                    info(" aus dem Nachrichtenkanal ")
-                    variableValue(channel.name)
-                    info(" geworfen.")
-                }
-
-                targetUser.sendPrefixed {
-                    info("Du wurdest aus dem Nachrichtenkanal ")
-                    variableValue(channel.name)
-                    info(" geworfen.")
-                }
+        if (!userMember.hasModeratorPermissions()) {
+            user.sendText {
+                error("Du verfügst nicht über die erforderliche Berechtigung.")
             }
+            return@playerExecutor
+        }
+
+        val targetMember = target.user()?.channelMember(channel) ?: return@playerExecutor run {
+            user.sendText {
+                appendPrefix()
+                error("Der Spieler ist nicht in deinem Nachrichtenkanal.")
+            }
+        }
+
+        if (targetMember.hasModeratorPermissions()) {
+            user.sendText {
+                error("Du kannst diesen Spieler nicht aus dem Nachrichtenkanal entfernen.")
+            }
+            return@playerExecutor
+        }
+
+
+        channel.kick(targetMember)
+        user.sendText {
+            info("Du hast ")
+            variableValue(targetMember.name)
+            info(" aus dem Nachrichtenkanal ")
+            variableValue(channel.channelName)
+            info(" geworfen.")
+        }
+
+        target.sendText {
+            info("Du wurdest aus dem Nachrichtenkanal ")
+            variableValue(channel.channelName)
+            info(" geworfen.")
         }
     }
 }

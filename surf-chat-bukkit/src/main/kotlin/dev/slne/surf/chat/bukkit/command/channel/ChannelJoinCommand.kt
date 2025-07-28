9e15bd002c0041c96a348c41237b9174341b6825
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-28 09:25:07 +0200

    feat: refactor ChannelJoinCommand for improved readability and consistency

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelJoinCommand.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelJoinCommand.kt
@@ -1,57 +1,52 @@
 package dev.slne.surf.chat.bukkit.command.channel
 
-import com.github.shynixn.mccoroutine.folia.launch
 import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.kotlindsl.getValue
 import dev.jorel.commandapi.kotlindsl.playerExecutor
-import dev.slne.surf.chat.api.model.ChannelModel
-import dev.slne.surf.chat.api.type.ChannelStatusType
-import dev.slne.surf.chat.bukkit.command.argument.ChannelArgument
-import dev.slne.surf.chat.bukkit.plugin
-import dev.slne.surf.chat.bukkit.util.ChatPermissionRegistry
-import dev.slne.surf.chat.bukkit.util.utils.sendPrefixed
+import dev.jorel.commandapi.kotlindsl.subcommand
+import dev.slne.surf.chat.api.model.Channel
+import dev.slne.surf.chat.api.model.ChannelVisibility
+import dev.slne.surf.chat.bukkit.command.argument.channelArgument
+import dev.slne.surf.chat.bukkit.permission.SurfChatPermissionRegistry
+import dev.slne.surf.chat.bukkit.util.sendText
+import dev.slne.surf.chat.bukkit.util.user
 import dev.slne.surf.chat.core.service.channelService
-import dev.slne.surf.chat.core.service.databaseService
 
-class ChannelJoinCommand(commandName: String) : CommandAPICommand(commandName) {
-    init {
-        withPermission(ChatPermissionRegistry.COMMAND_CHANNEL_JOIN)
-        withArguments(ChannelArgument("channel"))
-        playerExecutor { player, args ->
-            val channel = args.getUnchecked<ChannelModel>("channel") ?: return@playerExecutor
+fun CommandAPICommand.channelJoinCommand() = subcommand("join") {
+    withPermission(SurfChatPermissionRegistry.COMMAND_CHANNEL_JOIN)
+    channelArgument("channel")
+    playerExecutor { player, args ->
+        val channel: Channel by args
+        val user = player.user() ?: return@playerExecutor
 
-            plugin.launch {
-                val user = databaseService.getUser(player.uniqueId)
-
-                if (channelService.getChannel(player) != null) {
-                    user.sendPrefixed {
-                        error("Du bist bereits in einem Nachrichtenkanal.")
-                    }
-                    return@launch
-                }
-
-                if (channel.isBanned(user)) {
-                    user.sendPrefixed {
-                        error("Du wurdest von diesem Nachrichtenkanal ausgeschlossen.")
-                    }
-                    return@launch
-                }
-
-                if (channel.status != ChannelStatusType.PUBLIC && !channel.isInvited(user)) {
-                    user.sendPrefixed {
-                        error("Der Nachrichtenkanal ")
-                        variableValue(channel.name)
-                        error(" ist privat.")
-                    }
-                    return@launch
-                }
-
-                channel.join(user)
-                user.sendPrefixed {
-                    info("Du bist dem Nachrichtenkanal ")
-                    variableValue(channel.name)
-                    info(" beigetreten.")
-                }
+        if (channelService.getChannel(user) != null) {
+            user.sendText {
+                error("Du bist bereits in einem Nachrichtenkanal.")
             }
+            return@playerExecutor
+        }
+
+        if (channel.isBanned(user)) {
+            user.sendText {
+                error("Du wurdest von diesem Nachrichtenkanal ausgeschlossen.")
+            }
+            return@playerExecutor
+        }
+
+        if (channel.visibility != ChannelVisibility.PUBLIC && !channel.isInvited(user)) {
+            user.sendText {
+                error("Der Nachrichtenkanal ")
+                variableValue(channel.channelName)
+                error(" ist privat.")
+            }
+            return@playerExecutor
+        }
+
+        channel.addMember(user)
+        user.sendText {
+            info("Du bist dem Nachrichtenkanal ")
+            variableValue(channel.channelName)
+            info(" beigetreten.")
         }
     }
 }

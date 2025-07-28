3991cb43940b2ccbf8789598e79f2d6b80474727
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-28 11:08:41 +0200

    feat: refactor ChannelMembersCommand for improved command structure and pagination

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelMembersCommand.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelMembersCommand.kt
@@ -3,74 +3,60 @@ package dev.slne.surf.chat.bukkit.command.channel
 import dev.jorel.commandapi.CommandAPICommand
 import dev.jorel.commandapi.kotlindsl.integerArgument
 import dev.jorel.commandapi.kotlindsl.playerExecutor
-
-import dev.slne.surf.chat.api.model.ChannelModel
-import dev.slne.surf.chat.api.surfChatApi
-import dev.slne.surf.chat.bukkit.command.argument.ChannelArgument
-import dev.slne.surf.chat.bukkit.util.ChatPermissionRegistry
-import dev.slne.surf.chat.bukkit.util.PageableMessageBuilder
-import dev.slne.surf.chat.bukkit.util.utils.sendPrefixed
+import dev.jorel.commandapi.kotlindsl.subcommand
+import dev.slne.surf.chat.api.entity.ChannelMember
+import dev.slne.surf.chat.api.model.Channel
+import dev.slne.surf.chat.bukkit.command.argument.channelArgument
+import dev.slne.surf.chat.bukkit.permission.SurfChatPermissionRegistry
+import dev.slne.surf.chat.bukkit.util.user
 import dev.slne.surf.chat.core.service.channelService
 import dev.slne.surf.surfapi.core.api.font.toSmallCaps
+import dev.slne.surf.surfapi.core.api.messages.CommonComponents
 import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import dev.slne.surf.surfapi.core.api.messages.pagination.Pagination
 import net.kyori.adventure.text.format.TextDecoration
 
-class ChannelMembersCommand(commandName: String) : CommandAPICommand(commandName) {
-    init {
-        withArguments(ChannelArgument("channel").setOptional(true))
-        integerArgument("page", 1, optional = true)
-        withPermission(ChatPermissionRegistry.COMMAND_CHANNEL_MEMBERS)
-        playerExecutor { player, args ->
-            val page = args.getOrDefaultUnchecked("page", 1)
-            val channel: ChannelModel? =
-                args.getOrDefaultUnchecked("channel", channelService.getChannel(player))
+fun CommandAPICommand.channelMembersCommand() = subcommand("members") {
+    withPermission(SurfChatPermissionRegistry.COMMAND_CHANNEL_MEMBERS)
+    channelArgument("channel", true)
+    integerArgument("page", 1, optional = true)
+    playerExecutor { player, args ->
+        val page = args.getOrDefaultUnchecked("page", 1)
+        val user = player.user() ?: return@playerExecutor
+        val channel: Channel? =
+            args.getOrDefaultUnchecked("channel", channelService.getChannel(user))
 
-            if (channel == null) {
-                player.sendPrefixed {
-                    error("Du bist in keinem Nachrichtenkanal oder der Kanal existiert nicht.")
-                }
-                return@playerExecutor
+        if (channel == null) {
+            player.sendText {
+                appendPrefix()
+                error("Du bist in keinem Nachrichtenkanal oder der Kanal existiert nicht.")
+            }
+            return@playerExecutor
+        }
+
+        val pagination = Pagination<ChannelMember> {
+            title {
+                primary("Kanalmitglieder".toSmallCaps(), TextDecoration.BOLD)
             }
 
-            PageableMessageBuilder {
-                pageCommand = "/channel members ${channel.name} %page%"
-
-                title {
-                    info("Mitglider von ".toSmallCaps())
-                    variableValue(channel.name)
-                }
-
-                line {
-                    append {
-                        info("| ")
-                        decorate(TextDecoration.BOLD)
+            rowRenderer { member, index ->
+                listOf(
+                    buildText {
+                        append(CommonComponents.EM_DASH)
+                        appendSpace()
+                        variableKey(member.name)
+                        appendSpace()
+                        spacer("(")
+                        variableValue(member.role.name.replaceFirstChar { it.uppercase() })
+                        spacer(")")
                     }
-                    variableValue(channel.getOwner().getName())
-                    darkSpacer(" (Besitzer)")
-                }
+                )
+            }
+        }
 
-                channel.getModerators().forEach {
-                    line {
-                        append {
-                            info("| ")
-                            decorate(TextDecoration.BOLD)
-                        }
-                        variableValue(it.getName())
-                        darkSpacer(" (Moderator)")
-                    }
-                }
-
-                channel.getMembers(false).forEach {
-                    line {
-                        append {
-                            info("| ")
-                            decorate(TextDecoration.BOLD)
-                        }
-                        variableValue(it.getName())
-                        darkSpacer(" (Mitglied)")
-                    }
-                }
-            }.send(player, page)
+        player.sendText {
+            append(pagination.renderComponent(channel.members.sortedByDescending { it.role }, page))
         }
     }
 }

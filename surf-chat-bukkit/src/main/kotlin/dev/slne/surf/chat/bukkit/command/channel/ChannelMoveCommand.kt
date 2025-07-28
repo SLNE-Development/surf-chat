97018a0f0f02bd768efc791cab262d4ccef99839
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-28 11:50:36 +0200

    feat: refactor ChannelMoveCommand for improved command structure and user feedback

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelMoveCommand.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/channel/ChannelMoveCommand.kt
@@ -1,55 +1,51 @@
 package dev.slne.surf.chat.bukkit.command.channel
 
-import com.github.shynixn.mccoroutine.folia.launch
 import dev.jorel.commandapi.CommandAPICommand
-import dev.jorel.commandapi.arguments.ArgumentSuggestions
-import dev.jorel.commandapi.arguments.EntitySelectorArgument
+import dev.jorel.commandapi.kotlindsl.getValue
+import dev.jorel.commandapi.kotlindsl.playerArgument
 import dev.jorel.commandapi.kotlindsl.playerExecutor
-import dev.slne.surf.chat.api.model.ChannelModel
-import dev.slne.surf.chat.bukkit.command.argument.ChannelArgument
-import dev.slne.surf.chat.bukkit.plugin
-import dev.slne.surf.chat.bukkit.util.ChatPermissionRegistry
-import dev.slne.surf.chat.bukkit.util.utils.sendPrefixed
+import dev.jorel.commandapi.kotlindsl.subcommand
+import dev.slne.surf.chat.api.model.Channel
+import dev.slne.surf.chat.bukkit.command.argument.channelArgument
+import dev.slne.surf.chat.bukkit.permission.SurfChatPermissionRegistry
+import dev.slne.surf.chat.bukkit.util.sendText
+import dev.slne.surf.chat.bukkit.util.user
 import dev.slne.surf.chat.core.service.channelService
-import dev.slne.surf.chat.core.service.databaseService
-import org.bukkit.Bukkit
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
 import org.bukkit.entity.Player
 
-class ChannelMoveCommand(commandName: String) : CommandAPICommand(commandName) {
-    init {
-        withPermission(ChatPermissionRegistry.COMMAND_CHANNEL_ADMIN_MOVE)
-        withArguments(EntitySelectorArgument.OnePlayer("player").replaceSuggestions(
-            ArgumentSuggestions.stringCollection {
-                val players = Bukkit.getOnlinePlayers().map { it.name }
-                players.toSet()
-            }))
-        withArguments(ChannelArgument("channel"))
-        withPermission("surf.chat.command.channel.move")
+fun CommandAPICommand.channelMoveCommand() = subcommand("move") {
+    withPermission(SurfChatPermissionRegistry.COMMAND_CHANNEL_ADMIN_MOVE)
+    playerArgument("target")
+    channelArgument("channel")
+    playerExecutor { player, args ->
+        val target: Player by args
+        val channel: Channel by args
 
-        playerExecutor { player, args ->
-            val target = args.getUnchecked<Player>("player") ?: return@playerExecutor
-            val channel = args.getUnchecked<ChannelModel>("channel") ?: return@playerExecutor
-
-            plugin.launch {
-                val user = databaseService.getUser(player.uniqueId)
-                val targetUser = databaseService.getUser(target.uniqueId)
-
-                channelService.move(target, channel)
-
-                user.sendPrefixed {
-                    success("Du hast ")
-                    variableValue(targetUser.getName())
-                    success(" in den Nachrichtenkanal ")
-                    variableValue(channel.name)
-                    success(" verschoben.")
-                }
-
-                targetUser.sendPrefixed {
-                    info("Du wurdest in den Nachrichtenkanal ")
-                    variableValue(channel.name)
-                    info(" verschoben.")
-                }
+        val targetUser = target.user() ?: run {
+            player.sendText {
+                appendPrefix()
+                error("Der Spieler ist nicht online oder existiert nicht.")
             }
+            return@playerExecutor
+        }
+
+        channelService.move(targetUser, channel)
+
+        player.sendText {
+            appendPrefix()
+            success("Du hast ")
+            variableValue(targetUser.name)
+            success(" in den Nachrichtenkanal ")
+            variableValue(channel.channelName)
+            success(" verschoben.")
+        }
+
+        targetUser.sendText {
+            appendPrefix()
+            info("Du wurdest in den Nachrichtenkanal ")
+            variableValue(channel.channelName)
+            info(" verschoben.")
         }
     }
 }

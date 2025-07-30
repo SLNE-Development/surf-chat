9c6aa0af5c8430db736de7e34d22950fb9254429
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-30 10:48:58 +0200

    feat: adjust chat MOTD message formatting for improved display

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/config/ChatMotdConfig.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/config/ChatMotdConfig.kt
@@ -5,5 +5,5 @@ import org.spongepowered.configurate.objectmapping.ConfigSerializable
 @ConfigSerializable
 data class ChatMotdConfig(
     val enabled: Boolean = true,
-    val message: String = "<dark_gray>--------------------------------------------------<br><br><#f9c353>Willkommen auf dem CastCrafter Community Server!<br><br><#96CFE8>Alle Informationen, inklusive der Serverregeln, findest du auf:<br><#4BB9F9>https://castcrafter.de/server<br><br><dark_gray>--------------------------------------------------\n"
+    val message: String = "<br><dark_gray>--------------------------------------------------<br><br><#f9c353>Willkommen auf dem CastCrafter Community Server!<br><br><#96CFE8>Alle Informationen, inklusive der Serverregeln, findest du auf:<br><#4BB9F9>https://castcrafter.de/server<br><br><dark_gray>--------------------------------------------------\n"
 )

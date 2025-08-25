ba90b71ccb7bfddda5229d65fe017cf3e049d6ef
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-08-25 20:04:02 +0200

    feat: update primary key in FunctionalityTable to use server instead of id

--- surf-chat-fallback/src/main/kotlin/dev/slne/surf/chat/fallback/table/FunctionalityTable.kt
+++ surf-chat-fallback/src/main/kotlin/dev/slne/surf/chat/fallback/table/FunctionalityTable.kt
@@ -3,9 +3,8 @@ package dev.slne.surf.chat.fallback.table
 import org.jetbrains.exposed.sql.Table
 
 object FunctionalityTable : Table("chat_functionality") {
-    val index = integer("id").autoIncrement()
     val server = varchar("server", 256)
     val chatEnabled = bool("chat_enabled").default(true)
 
-    override val primaryKey = PrimaryKey(index)
+    override val primaryKey = PrimaryKey(server)
 }

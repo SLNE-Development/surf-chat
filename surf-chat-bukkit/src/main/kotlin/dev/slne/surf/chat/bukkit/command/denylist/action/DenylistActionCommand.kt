ac071fbdea720032ddc55745375bf2c3a856b027
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-09-10 17:03:00 +0200

    refactor: rename `denylist` command to `denylistaction` for improved clarity and alignment

--- surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/denylist/action/DenylistActionCommand.kt
+++ surf-chat-bukkit/src/main/kotlin/dev/slne/surf/chat/bukkit/command/denylist/action/DenylistActionCommand.kt
@@ -3,7 +3,7 @@ package dev.slne.surf.chat.bukkit.command.denylist.action
 import dev.jorel.commandapi.kotlindsl.commandAPICommand
 import dev.slne.surf.chat.bukkit.permission.SurfChatPermissionRegistry
 
-fun denylistActionCommand() = commandAPICommand("denylist") {
+fun denylistActionCommand() = commandAPICommand("denylistaction") {
     withPermission(SurfChatPermissionRegistry.COMMAND_DENYLIST_ACTION)
 
     denylistActionAddCommand()

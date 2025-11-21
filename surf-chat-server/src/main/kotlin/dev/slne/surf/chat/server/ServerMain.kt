1ab43dd88e8d2ef49f8186992b7c13f3f5ff63e1
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-11-22 00:46:22 +0100

    feat: update ServerMain to utilize Denylist repositories for action caching and storage

--- surf-chat-server/src/main/kotlin/dev/slne/surf/chat/server/ServerMain.kt
+++ surf-chat-server/src/main/kotlin/dev/slne/surf/chat/server/ServerMain.kt
@@ -1,14 +1,26 @@
 package dev.slne.surf.chat.server
 
+import dev.slne.surf.chat.server.database.repository.DenylistActionRepository
+import dev.slne.surf.chat.server.database.repository.DenylistRepository
 import dev.slne.surf.cloud.api.server.plugin.StandalonePlugin
 import dev.slne.surf.surfapi.core.api.util.logger
 
-class ServerMain : StandalonePlugin() {
+class ServerMain(
+    private val denylistRepository: DenylistRepository,
+    private val denylistActionRepository: DenylistActionRepository
+) : StandalonePlugin() {
     private val pluginLogger = logger()
 
     override suspend fun load() {}
-    override suspend fun enable() {}
-    override suspend fun disable() {}
+    override suspend fun enable() {
+        denylistActionRepository.cacheActions()
+        denylistRepository.cacheDenylist()
+    }
+
+    override suspend fun disable() {
+        denylistActionRepository.storeActions()
+        denylistRepository.storyDenylist()
+    }
 }
 
 val plugin get() = StandalonePlugin.getPlugin(ServerMain::class)

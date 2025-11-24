f4b01062090d157aff5eb547c9bf6e471a20894c
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-11-24 21:03:38 +0100

    feat: refactor ignore command to use Deferred for offline player handling

--- surf-chat-paper/src/main/kotlin/dev/slne/surf/chat/paper/command/ignore/IgnoreComand.kt
+++ surf-chat-paper/src/main/kotlin/dev/slne/surf/chat/paper/command/ignore/IgnoreComand.kt
@@ -11,23 +11,26 @@ import dev.slne.surf.chat.paper.plugin
 import dev.slne.surf.cloud.api.client.paper.command.args.offlineCloudPlayerArgument
 import dev.slne.surf.cloud.api.common.player.OfflineCloudPlayer
 import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import kotlinx.coroutines.Deferred
 
 fun ignoreCommand() = commandAPICommand("ignore", plugin) {
     withPermission(SurfChatPermissionRegistry.COMMAND_IGNORE)
     ignoreListCommand()
     offlineCloudPlayerArgument("target")
     playerExecutor { player, args ->
-        val target: OfflineCloudPlayer by args
-
-        if (player.uniqueId == target.uuid) {
-            player.sendText {
-                appendPrefix()
-                error("Du kannst dich nicht selbst ignorieren.")
-            }
-            return@playerExecutor
-        }
+        val deferred: Deferred<OfflineCloudPlayer> by args
 
         plugin.launch {
+            val target = deferred.await()
+
+            if (player.uniqueId == target.uuid) {
+                player.sendText {
+                    appendPrefix()
+                    error("Du kannst dich nicht selbst ignorieren.")
+                }
+                return@launch
+            }
+
             if (ignorelistService.isIgnoring(player.uniqueId, target.uuid)) {
                 ignorelistService.removeFromIgnoreList(player.uniqueId, target.uuid)
 

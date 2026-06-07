d50a2721f0892f1773e684e9990cfd716eb88ccb
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-06-07 20:36:23 +0200

    ✨ refactor(DeleteRemoteMessageRedisEvent): change messageSignature type to SerializableSignature for improved serialization

--- surf-chat-paper/src/main/kotlin/dev/slne/surf/chat/paper/redis/event/DeleteRemoteMessageRedisEvent.kt
+++ surf-chat-paper/src/main/kotlin/dev/slne/surf/chat/paper/redis/event/DeleteRemoteMessageRedisEvent.kt
@@ -1,10 +1,10 @@
 package dev.slne.surf.chat.paper.redis.event
 
+import dev.slne.surf.chat.api.serializer.SerializableSignature
 import dev.slne.surf.redis.event.RedisEvent
 import kotlinx.serialization.Serializable
-import net.kyori.adventure.chat.SignedMessage
 
 @Serializable
 data class DeleteRemoteMessageRedisEvent(
-    val messageSignature: SignedMessage.Signature
+    val messageSignature: SerializableSignature
 ) : RedisEvent()
